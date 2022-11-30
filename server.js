531241bd204614661ae937ac24a6de0d2997a91a
Karina Drummond <k.drummond528@gmail.com>
2022-11-30 13:21:10 -0800

    modified server.js

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
 
 // routes
-const api = require('./routes/apiroutes');
-const html = require('./routes/htmlroutes');
+const api = require('./routes/apiRoutes');
+const html = require('./routes/htmlRoutes');
 
 // middleware
 app.use(express.json());
