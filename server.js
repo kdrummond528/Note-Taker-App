78c5062d0246c793a48e788906d1eb29c706778b
Karina Drummond <k.drummond528@gmail.com>
2022-11-30 13:26:10 -0800

    modified server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// dependencies
 const express = require('express');
 const path = require('path');
 
@@ -7,16 +8,18 @@ const app = express();
 // local host
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static('public'));
-
 // routes
 const api = require('./routes/apiRoutes');
+app.use(apiRoutes);
 const html = require('./routes/htmlRoutes');
+app.use(htmlRoutes);
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
+// server listener
 app.listen(PORT, () => {
     console.log("Listening on PORT", PORT)
 });
